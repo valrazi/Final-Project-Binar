f0fa9a19db6473530406e8793eda5f206eb6149e
valrazi <ivallavif@gmail.com>
2023-06-30 09:52:08 +0700

    Update admin controller

---
+++ src/main/java/org/bejb4/finalproject/repository/KelasRepository.java
@@ -0,0 +1,7 @@
+package org.bejb4.finalproject.repository;
+
+import org.bejb4.finalproject.model.Kelas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KelasRepository extends JpaRepository<Kelas, Integer> {
+}
--- src/main/java/org/bejb4/finalproject/service/JadwalService.java
+++ src/main/java/org/bejb4/finalproject/service/JadwalService.java
@@ -1,7 +1,9 @@
 package org.bejb4.finalproject.service;
 
 import org.bejb4.finalproject.model.Jadwal;
+import org.bejb4.finalproject.model.Kelas;
 import org.bejb4.finalproject.repository.JadwalRepository;
+import org.bejb4.finalproject.repository.KelasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -9,12 +11,16 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class JadwalService {
     @Autowired
     private JadwalRepository jadwalRepository;
+
+    @Autowired
+    private KelasRepository kelasRepository;
     public List<Jadwal> getAllJadwal(){
         return jadwalRepository.findAll();
     }
@@ -33,27 +39,40 @@ public class JadwalService {
         return null;
     }
 
-    public Jadwal addJadwal(Jadwal jadwal){
-        Jadwal newJadwal = new Jadwal();
-        newJadwal.setTglKeberangkatan(jadwal.getTglKeberangkatan());
-        newJadwal.setTglKedatangan(jadwal.getTglKedatangan());
-        newJadwal.setJamKeberangkatan(jadwal.getJamKeberangkatan());
-        newJadwal.setJamKedatangan(jadwal.getJamKedatangan());
-        newJadwal.setHargaTiket(jadwal.getHargaTiket());
-        LocalTime jamKeberangkatan = jadwal.getJamKeberangkatan();
-        LocalTime jamKedatangan = jadwal.getJamKedatangan();
-        int hoursDurasi = jamKedatangan.minusHours(jamKeberangkatan.getHour()).getHour();
-        int minuteDurasi = jamKedatangan.minusMinutes(jamKeberangkatan.getMinute()).getMinute();
-        newJadwal.setDurasiJam(hoursDurasi);
-        newJadwal.setDurasiMenit(minuteDurasi);
+    public List<Jadwal> addJadwal(Jadwal jadwal){
+
+        List<Jadwal> jadwalList = new ArrayList<Jadwal>();
+        for (int i = 1; i <= 4; i++) {
+            Jadwal newJadwal = new Jadwal();
+            newJadwal.setTglKeberangkatan(jadwal.getTglKeberangkatan());
+            newJadwal.setTglKedatangan(jadwal.getTglKedatangan());
+            newJadwal.setJamKeberangkatan(jadwal.getJamKeberangkatan());
+            newJadwal.setJamKedatangan(jadwal.getJamKedatangan());
+            LocalTime jamKeberangkatan = jadwal.getJamKeberangkatan();
+            LocalTime jamKedatangan = jadwal.getJamKedatangan();
+            int hoursDurasi = jamKedatangan.minusHours(jamKeberangkatan.getHour()).getHour();
+            int minuteDurasi = jamKedatangan.minusMinutes(jamKeberangkatan.getMinute()).getMinute();
+            newJadwal.setDurasiJam(hoursDurasi);
+            newJadwal.setDurasiMenit(minuteDurasi);
 
 
-        newJadwal.setKotaKeberangkatan(jadwal.getKotaKeberangkatan());
-        newJadwal.setKotaKedatangan(jadwal.getKotaKedatangan());
+            newJadwal.setKotaKeberangkatan(jadwal.getKotaKeberangkatan());
+            newJadwal.setKotaKedatangan(jadwal.getKotaKedatangan());
 
-        newJadwal.setMaskapai(jadwal.getMaskapai());
-
-        newJadwal.setKelas(jadwal.getKelas());
-        return jadwalRepository.save(newJadwal);
+            newJadwal.setMaskapai(jadwal.getMaskapai());
+            Kelas kelas = kelasRepository.findById(i).orElseThrow(() -> new IllegalArgumentException("kelas not foubd"));
+            if(i == 1){
+                newJadwal.setHargaTiket(jadwal.getHargaTiket());
+            }else if(i == 2){
+                newJadwal.setHargaTiket(jadwal.getHargaTiket() * 2);
+            }else if(i == 3){
+                newJadwal.setHargaTiket(jadwal.getHargaTiket() * 4);
+            }else if(i == 4){
+                newJadwal.setHargaTiket(jadwal.getHargaTiket() * 8);
+            }
+            newJadwal.setKelas(kelas);
+            jadwalList.add(newJadwal);
+        }
+        return jadwalRepository.saveAll(jadwalList);
     }
 }
